57f895098a0de4ebbb2e4025caedb1ed8d455b51
yogahmad <pratamay735@gmail.com>
2025-02-21 08:37:49 +0700

    first commit

---
+++ src/main/java/com/example/tic_tac_toe/TicTacToeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tic_tac_toe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToeApplication.class, args);
+	}
+
+}
